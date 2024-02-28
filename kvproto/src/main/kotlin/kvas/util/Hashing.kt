8c04f06de18ff43845e909fc688f81702921e6dc
Dmitry Barashev <dbarashev@ganttproject.biz>
2024-02-28 18:55:35 +0400

    Some doc comments

--- kvproto/src/main/kotlin/kvas/util/Hashing.kt
+++ kvproto/src/main/kotlin/kvas/util/Hashing.kt
@@ -8,12 +8,21 @@ import kotlin.math.roundToInt
 
 fun hashCode(key: String) = Hashing.murmur3_32().newHasher().putString(key, Charset.defaultCharset()).hash().asInt().toUInt()
 
+/**
+ * This object encapsulates functions for linear hashing scheme.
+ */
 object LinearHashing {
+  /**
+   * Clears the highest significant bit of this integer.
+   */
   fun Int.clearHighestBit() = this.toUInt().and((this.takeHighestOneBit() - 1).toUInt()).toInt()
 
-  fun bitmask(num: Int): UInt =
+  private fun bitmask(num: Int): UInt =
     ((1 shl log2(num.toDouble()).roundToInt()) - 1).toUInt()
 
+  /**
+   * Calculates a shard number from the hash code and total shard count using linear hashing method.
+   */
   fun shardNumber(key: String, shardCount: Int): Int {
     val lastBits = hashCode(key).and(bitmask(shardCount)).toInt()
     return if (lastBits < shardCount) {
@@ -23,6 +32,9 @@ object LinearHashing {
     }
   }
 
+  /**
+   * Returns a number of a shard that is going to split after adding shard number shardNumber.
+   */
   fun splitShardNumber(shardNumber: Int) = shardNumber.clearHighestBit()
 }
 
