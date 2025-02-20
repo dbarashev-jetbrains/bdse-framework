0a7fef1adfda3a99c6871bf370fa9f5da2ed74cd
Dmitry.Barashev <dmitry.barashev@jetbrains.com>
2025-02-20 20:43:55 +0400

    sleep before initializing postgreses

--- test-initialize-postgres.sh
+++ test-initialize-postgres.sh
@@ -8,6 +8,7 @@ docker run -d -p 5430:5432 --name kvas-postgres-0 -e POSTGRES_HOST_AUTH_METHOD=t
 docker run -d -p 5431:5432 --name kvas-postgres-1 -e POSTGRES_HOST_AUTH_METHOD=trust postgres
 docker run -d -p 5432:5432 --name kvas-postgres-2 -e POSTGRES_HOST_AUTH_METHOD=trust postgres
 
+sleep 5
 echo "Initializing PostgreSQL..."
 psql -h localhost -U postgres -p 5430 -f kvnode/src/main/resources/postgres-init.sql
 psql -h localhost -U postgres -p 5431 -f kvnode/src/main/resources/postgres-init.sql
