7aeebcbd169ac16099e41bc7dd5ca4f1744d903f
Dmitry.Barashev <dmitry.barashev@jetbrains.com>
2025-03-27 19:45:01 +0400

    Mapper interface, Reducer service draft

--- kvnode/src/test/kotlin/kvas/node/mapreduce/MapperTest.kt
+++ kvnode/src/test/kotlin/kvas/node/mapreduce/MapperTest.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.runBlocking
 import kvas.node.storage.DEFAULT_COLUMN_NAME
 import kvas.node.storage.InMemoryStorage
 import kvas.proto.startMapRequest
+import kvas.util.NodeAddress
 import org.junit.jupiter.api.Test
 import java.util.concurrent.Executors
 
@@ -14,7 +15,7 @@ class MapperTest {
         val storage = InMemoryStorage()
         storage.put("line1", DEFAULT_COLUMN_NAME, "lorem ipsum dolor sit amet")
         storage.put("line2", DEFAULT_COLUMN_NAME, "consectetur adipiscing elit")
-        val mapper = MapperImpl(storage, Runnable::run)
+        val mapper = MapperImpl(NodeAddress("127.0.0.1", 9000), storage, DemoMapperImpl(), Runnable::run)
         runBlocking {
             mapper.startMap(startMapRequest {  })
         }
