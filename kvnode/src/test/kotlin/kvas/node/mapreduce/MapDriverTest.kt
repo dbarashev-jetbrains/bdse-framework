f48f63f3efe2f9a58e591aa470a48112047564e6
Dmitry Barashev <dmitry.barashev@jetbrains.com>
2025-04-01 00:21:25 +0400

    Fixed map test

--- kvnode/src/test/kotlin/kvas/node/mapreduce/MapDriverTest.kt
+++ kvnode/src/test/kotlin/kvas/node/mapreduce/MapDriverTest.kt
@@ -3,11 +3,12 @@ package kvas.node.mapreduce
 import kotlinx.coroutines.runBlocking
 import kvas.node.storage.DEFAULT_COLUMN_NAME
 import kvas.node.storage.InMemoryStorage
-import kvas.proto.startMapRequest
+import kvas.proto.*
+import kvas.proto.KvasSharedProto.DataRow
 import kvas.setup.AllShardings
-import kvas.setup.Sharding
 import kvas.util.NodeAddress
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class MapDriverTest {
     @Test
@@ -15,9 +16,46 @@ class MapDriverTest {
         val storage = InMemoryStorage()
         storage.put("line1", DEFAULT_COLUMN_NAME, "lorem ipsum dolor sit amet")
         storage.put("line2", DEFAULT_COLUMN_NAME, "consectetur adipiscing elit")
-        val mapper = MapperImpl(NodeAddress("127.0.0.1", 9000), storage, DemoMapDriver(storage, AllShardings.NAIVE.second), Runnable::run)
+        
+        val mapOutputStorage = InMemoryStorage()
+        val mapper = MapperImpl(
+            NodeAddress("127.0.0.1", 9000),
+            storage,
+            DemoMapDriver(mapOutputStorage, AllShardings.NAIVE.second),
+            Runnable::run,
+            { _, _ -> addMapOutputShardResponse {} }
+        )
         runBlocking {
-            mapper.startMap(startMapRequest {  })
+            mapper.startMap(startMapRequest {
+                metadata = createMetadata(1)
+                mapFunction = """
+                    fun mapper(rowKey: String, values: Map<String, String>): List<Pair<String, Any?>> {
+                      return listOf(rowKey to 1)
+                    }
+                """.trimIndent()
+            })
         }
+        val outputShard: Map<String, Any> = mapOutputStorage.scan().use {
+            it.asSequence().associate<DataRow, String, Any> {
+                row -> row.key to row.valuesMap[DEFAULT_COLUMN_NAME]!!
+            }
+        }
+        assertEquals<Map<String, Any>>(
+            mapOf("line1" to "1", "line2" to "1"),
+            outputShard
+        )
+    }
+}
+
+private fun createMetadata(shardCount: Int) = clusterMetadata {
+    (0 until shardCount).forEach { index ->
+        shards.add(replicatedShard {
+            leader = nodeInfo {
+                nodeAddress = "127.0.0.1:900$index"
+                isReplica = false
+                shardToken = index
+            }
+            shardToken = index
+        })
     }
 }
